4701a5882b6760a1011cdeaefaee2e5fb4527762
JHenrysson <56089745+JHenrysson@users.noreply.github.com>
2020-10-18 22:03:48 +0200

    function set up
    set up of functions

--- Lab3_1.py
+++ Lab3_1.py
@@ -1,9 +1,35 @@
+import sys
 
 
 def read_file(filename):
+    try:
+        log_file = open(filename, 'r')
+    except:
+        # if the file is not valid then this message will be printed
+        print(f"Error: The file '{filename}' could not be found.")
+        exit()
 
 def map_to_int(measurements):
 
-def find_faulty(primary,secondary,threshold):
 
-def main ():
+def find_faulty(primary, secondary, threshold):
+
+
+def display_warnings(faulty_sensors):
+    pass
+
+
+def main():
+    filename = sys.argvs[1]
+    filename1 = sys.argvs[2]
+    measurements = read_file(filename)
+    primary = map_to_int(measurements)
+    measurement2nd = read_file(filename1)
+    secondary = map_to_int(measurement2nd)
+    threshold = 2
+    faulty_sensors = find_faulty(primary, secondary, threshold)
+    display_warnings(faulty_sensors)
+
+
+if __name__ == '__main__':
+    main()
