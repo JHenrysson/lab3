b8bebb23947a5f7774fc3960a7318c3e905d1fc4
JHenrysson <56089745+JHenrysson@users.noreply.github.com>
2020-10-19 21:36:28 +0200

    read file using pickle

--- Lab3_1.py
+++ Lab3_1.py
@@ -1,27 +1,42 @@
 import sys
-
+import pickle
 
 def read_file(filename):
+    # tries to open the file
     try:
-        log_file = open(filename, 'r')
+        file =open(filename,"rb")
+    # if it cannot then prints error
     except:
-        # if the file is not valid then this message will be printed
-        print(f"Error: The file '{filename}' could not be found.")
+        print(f"An error occurred while trying to read the file.")
         exit()
+    # returns file contents as dictionary
+    return pickle.load(file)
+
+
+
+
+
+
 
 def map_to_int(measurements):
+    # a for loop to go through the dictionary
+    for key, value in measurements.items():
+        # slices last character of value and turns in to an int
+        measurements[key] = int(value[:-1])
+
+    return measurements
 
 
 def find_faulty(primary, secondary, threshold):
-
+    pass
 
 def display_warnings(faulty_sensors):
     pass
 
 
 def main():
-    filename = sys.argvs[1]
-    filename1 = sys.argvs[2]
+    filename = sys.argv[1]
+    filename1 = sys.argv[2]
     measurements = read_file(filename)
     primary = map_to_int(measurements)
     measurement2nd = read_file(filename1)
