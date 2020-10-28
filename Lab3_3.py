99d92a9f48d914eed652ec52b0ef32b85549876c
JHenrysson <56089745+JHenrysson@users.noreply.github.com>
2020-10-28 09:03:35 +0100

    created the functions

---
+++ Lab3_3.py
@@ -0,0 +1,14 @@
+def display_menu():
+    print("1. Add file")
+    print("2. Calculate")
+
+def cross_reference(files):
+    return('Set')
+
+def map_numbers_to_names(numbers,filename):
+    return("list")
+
+def display_suspects(names):
+    print("The following persons was present on all crime scenes:")
+    print("-"*46)
+    print("suspects involved ")
