a115cfd7589889b2c94ff2e4979e590455f7c749
JHenrysson <56089745+JHenrysson@users.noreply.github.com>
2020-10-30 14:32:37 +0100

    format and comments

--- Lab3_3.py
+++ Lab3_3.py
@@ -7,18 +7,19 @@ def display_menu():
     print("2. Calculate")
 
 
+# an empty set that will hold the phone numbers
 def cross_reference(files):
+    # all the numbers in all the list
     numbers = set()
 
     for file in files:
-        temp = set()
-
+        # holds numbers from a specific file
+        numbers_in_file = set()
         try:
             file = open(file, "r")
         except:
             print(f"Error: There was a problem with at least one of the files.")
             exit()
-
         end = False
         # loops until the end of the file
         while not end:
@@ -26,19 +27,18 @@ def cross_reference(files):
             if not number:
                 end = True
             else:
-                temp.add(number.replace('\n', ''))
-
+                numbers_in_file.add(number.replace('\n', ''))
+        # if this is the first file -- no comparing is needed
         if len(numbers) == 0:
-            numbers = temp
+            numbers = numbers_in_file
         else:
-            numbers = numbers.intersection(temp)
-
+            # it checks what numbers are in both files
+            numbers = numbers.intersection(numbers_in_file)
     return numbers
 
 
 def map_numbers_to_names(numbers, filename):
     names = []
-
     try:
         file = open(filename, "rb")
         # if it cannot then prints error
@@ -47,7 +47,6 @@ def map_numbers_to_names(numbers, filename):
         exit()
 
     dict = pickle.load(file)
-
     for number in numbers:
         if number in dict:
             names.append(dict[number])
@@ -57,11 +56,14 @@ def map_numbers_to_names(numbers, filename):
 
 
 def display_suspects(names):
+    # if there are common vcalues
     print("The following persons was present on all crime scenes:")
-    print("-" * 46)
-    print("suspects involved ")
-    for name in names:
-        print(f'sdskdskdskdks: {name}')
+    print("-" * 54)
+    if len(names) == 0:
+        print("No matches")
+    else:
+        for name in names:
+            print(f'{name}')
 
 
 def main():
@@ -72,11 +74,13 @@ def main():
     while choice != '2':
         display_menu()
         choice = input("Enter choice: ")
-
+        # if user enters 1
         if choice == '1':
             name = input("Enter a filename (include full path): ")
+            # adds to the file list
             files.append(name)
         elif choice == '2':
+
             numbers = cross_reference(files)
             names = map_numbers_to_names(numbers, filename)
             display_suspects(names)
